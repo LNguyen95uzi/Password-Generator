10324f6b2d3c3dace9736eb337914ca9f57e5c97
Long Nguyen <Long.Nguyen@temple.edu>
2020-02-07 16:14:03 -0500

    Added all alerts and confirmations.

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const randomFunction = {
-  lower: getRandomLower;
-  upper: getRandomUpper;
-  number: getRandomNumber;
-  symbol: getRandomSymbol;
-}
+document.getElementById("generate").addEventListener("click", function() {
+  prompt("How many characters would you like your password to contain?");
+  confirm("Click OK to confirm including special characters.");
+  confirm("Click OK to confirm including numeric characters.");
+  confirm("Click OK to confirm including lowercase letters.");
+  confirm("Click OK to confirm including uppercase letters");
+
+
+})
+
+// const randomFunction = {
+//   lower: getRandomLower;
+//   upper: getRandomUpper;
+//   number: getRandomNumber;
+//   symbol: getRandomSymbol;
+// }
 
 
 
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
 
-function getRandomSymbol() {
-  const symbols = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~'; 
-  return symbols[Math.floor(Math.random() * symbols.length)];
+// function getRandomSymbol() {
+//   const symbols = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~'; 
+//   return symbols[Math.floor(Math.random() * symbols.length)];
 
-console.log(getRandomSymbol());
+// console.log(getRandomSymbol());
 
 
 
@@ -43,3 +53,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
